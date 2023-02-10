d53e41fb2921724d2348e3a1a03876df438dfd67
Sujin Han <hansujin1998@naver.com>
2023-02-11 01:24:52 +0900

    feat: Add controller and UI for showing login state in Navbar

--- board_project_front_sj/src/components/Navbar.js
+++ board_project_front_sj/src/components/Navbar.js
@@ -7,6 +7,7 @@ import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
 import logo from "../logo.svg";
 import LoginModal from "./LoginModal.js";
+import MyPageModal from "./MyPageModal.js";
 
 let Background = styled.div`
     background-color : powderblue;
@@ -44,6 +45,8 @@ let LoginModalBtn = styled.div`
 function Navbar() {
 
     let [loginModal, setLoginModal] = useState(false);
+    let [isLogin, setIsLogin] = useState(true);
+    let [myPageModal, setMyPageModal] = useState(false);
     let navigate = useNavigate();
 
     return (
@@ -56,11 +59,16 @@ function Navbar() {
                 </Link>
                 {/* <Logo onClick={() => {navigate("/")}}><LogoImg src={logo}/>Navbar</Logo>
                     -> 이런 식으로는? div에 navigate 써도 되나 */}
+                {
+                    !isLogin
+                    ? <LoginModalBtn onClick={() => { setLoginModal(!loginModal) }}>Login / Sign up</LoginModalBtn>
+                    : <LoginModalBtn onClick={() => { setMyPageModal(!myPageModal)}}>My Page / Logout</LoginModalBtn>
+                }
                 
-                <LoginModalBtn onClick={() => { setLoginModal(!loginModal) }}>Login / Sign up</LoginModalBtn>
             </Background>
             <div style={{ position : "relative" }}>
                 { loginModal && <LoginModal setLoginModal={setLoginModal}/> }
+                { myPageModal && <MyPageModal setMyPageModal={setMyPageModal}/> }
             </div>
             
         </div>
