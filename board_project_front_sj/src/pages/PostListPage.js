71221dc52f3ad5678804a157db7004cce7b3a2a1
Sujin Han <hansujin1998@naver.com>
2023-02-19 12:34:36 +0900

    feat: Get post title data from DB

--- board_project_front_sj/src/pages/PostListPage.js
+++ board_project_front_sj/src/pages/PostListPage.js
@@ -1,11 +1,36 @@
 /* eslint-disable */
 
+import styled from "styled-components";
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+let OutsideBox = styled.div`
+    width : 95%;
+    height : 780px;
+    border : 1px solid black;
+    margin : auto;
+    margin-top : 10px;
+`
+
 function PostListPage(props) {
+
+    let [realData, setRealData] = useState([]);
+    let pullData = () => {
+        axios.get("/list/post").then(data => {
+            let copy = [...realData];
+            copy = data.data
+            setRealData(copy);
+        }).catch(() => { console.log("게시글 데이터 가져오기 실패") });
+    }
+
+    // 페이지 mount시 1회 게시글 가져오는 기능
+    useEffect(() => { pullData(); }, [])
+
     return (
-        <div>
-            게시글 목록
-            {props.contents.map((data, i) => <div key={i}>{data.title}</div>)}
-        </div>
+        <OutsideBox>
+            <div>게시글 목록</div>
+            {realData.map((data, i) => <div key={i}>{data.title}</div> )}
+        </OutsideBox>
     )
 }
 
