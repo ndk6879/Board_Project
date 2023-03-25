e49788d4454fb4098175fc62787f5c9485a3352f
Donggyun Nam <ndk6879@naver.com>
2023-03-26 03:24:35 +0900

    feat: test and create RESTful API for DELETE request and PUT request to posts

--- server.js
+++ server.js
@@ -58,6 +58,34 @@ app.post('/test', (req, res) => {
     res.status(200).send({'message' : '없쪄!'})
 })
 
+app.put('/post/:id', (req, res) => {
+    db.collection('post').updateOne( 
+        {_id : parseInt(req.params.id)}, 
+        {$set : { title : req.body.title, content : req.body.content }}, 
+        (err, result) => {
+            console.log('req.body.title:',req.body.title)
+            console.log('req.body.content:',req.body.content)
+            console.log('req.body.id:',req.body.id)
+            // console.log('result:',result)
+            res.status(202).send({'message' : 'OK'})
+        }
+        );
+  });
+
+app.delete('/post/:id', (req, res) => {
+    req.body._id =  parseInt(req.params.id)
+    db.collection('post').deleteOne( 
+        {_id : parseInt(req.params.id)}, 
+        (err, result) => {
+            console.log('req.body.title:',req.body.title)
+            console.log('req.body.content:',req.body.content)
+            console.log('req.body.id:',req.body.id)
+            // console.log('result:',result)
+            res.status(204).send({'message' : 'OK', 'res' : result})
+        }
+        );
+}); 
+
 app.get('/list/post', (req, res) => {
     db.collection('post').find().toArray( (에러, 결과) => {
         res.json(결과)
@@ -197,36 +225,35 @@ function getPost() {
 }
 
 app.post('/post', (req,res) => {
-    db.collection('counter').findOne({name : '게시물갯수'}, function(에러, 결과){
-        var today = new Date();
-        var year = today.getFullYear();
-        var month = ('0' + (today.getMonth() + 1)).slice(-2);
-        var day = ('0' + today.getDate()).slice(-2);
-        var dateString = year + '/' + month  + '/' + day;
 
-        var hours = ('0' + today.getHours()).slice(-2); 
-        var minutes = ('0' + today.getMinutes()).slice(-2);
-        var seconds = ('0' + today.getSeconds()).slice(-2); 
+    var today = new Date();
+    var year = today.getFullYear();
+    var month = ('0' + (today.getMonth() + 1)).slice(-2);
+    var day = ('0' + today.getDate()).slice(-2);
+    var dateString = year + '/' + month  + '/' + day;
+    var hours = ('0' + today.getHours()).slice(-2); 
+    var minutes = ('0' + today.getMinutes()).slice(-2);
+    var seconds = ('0' + today.getSeconds()).slice(-2); 
+    var timeString = hours + ':' + minutes  + ':' + seconds; //포맷 = 15:47:29
 
-        var timeString = hours + ':' + minutes  + ':' + seconds; //포맷 = 15:47:29
+    getPost().then(function(data) { 
+        console.log('data:',data)
 
-        getPost().then(function(data) { 
-            console.log('data:',data)
-            var post_info = {
-                _id: data + 1, 
-                author : req.user.id, 
-                date : dateString, 
-                title : req.body.title, 
-                content : req.body.content, 
-                type : req.body.type, 
-                like : 0
-            }
-            db.collection('post').insertOne( post_info, (에러, 결과) => {
-                if(에러){return console.log(에러)}
-                else {res.status.send({'message':'OK'}); }
-            })
+        var post_info = {
+            _id: data + 1, 
+            author : req.user.id, 
+            date : dateString, 
+            title : req.body.title, 
+            content : req.body.content, 
+            type : req.body.type, 
+            like : 0
+        }
+
+        db.collection('post').insertOne( post_info, (에러, 결과) => {
+            res.status(201).send({'message':'OK'});
         })
     })
+    
 })
 
 app.get('/post/:id', (req, res) => {
@@ -253,12 +280,12 @@ app.get('/post', (req, res) => {
 })
 
 function 로그인했니(req, res, next) { 
-if (req.user) { 
-    next() 
-} 
-else { 
-    res.status(403).send({'message':'NO'}) 
-} 
+    if (req.user) { 
+        next() 
+    } 
+    else { 
+        res.status(403).send({'message':'NO'}) 
+    }
 } 
 
 /*
